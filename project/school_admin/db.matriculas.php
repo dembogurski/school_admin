417d4b02108b7f7244df1a50e6f0571360fd4a91
Doglas A. Dembogurski <dembogurski@gmail.com>
2017-09-28 20:14:09 -0400

    Subo estructura inicial

---
+++ project/school_admin/db.matriculas.php
@@ -0,0 +1,22 @@
+<?php
+/** project/school_admin/db.matriculas.php    ( db_form )
+ * 
+ * @author 	ycube RAD Plus ( automatically Generated ) 
+ * 
+ */ 
+
+$Obj->name = "matriculas";
+$Obj->alias = "Matriculas";
+$Obj->help = "Matriculas";
+$Obj->Inheritance = "";
+$Obj->File = "matriculas";
+$Obj->Filter = "";
+$Obj->Sort = "";
+$Obj->p_insert = "";
+$Obj->p_change = "";
+$Obj->p_delete = "";
+$Obj->Zebra = "white,lightblue";
+$Obj->Noedit = "";
+$Obj->NoInsert = "";
+$Obj->Limit = "";
+?>
